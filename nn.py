130674b1e9c77e34fb37c366bf9bdc9adac29113
Mateusz Furga <matfurga@gmail.com>
2024-08-22 16:45:12 +0200

    Update nn

--- nn.py
+++ nn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from typing import Callable
+from mnist_loader import mnist_load, raw_load
 
 import random
 import numpy as np
@@ -11,7 +12,7 @@ def sigmoid(z):
 def dsigmoid(z):
   return sigmoid(z) * (1. - sigmoid(z))
 
-class NN:
+class Network:
   def __init__(self, size: tuple):
     self.size = size
     self.num_layers = len(size)
@@ -28,42 +29,35 @@ class NN:
   @staticmethod
   def cost_derivative(y: np.array, v: np.array) -> np.array:
     """
-    Cost function for single training input.
-      y - result from the network (vector)
-      v - actual value (vector)
-
-      C(y, v) = 0.5 || y - v || ^ 2 = 0.5 * sum[ yi - vi ] ^ 2
-      grad C = y - v
+    Derivative of cost function C(y, v).
+    C(y, v) = 0.5 || y - v || ^ 2 = 0.5 * sum[ yi - vi ] ^ 2
     """
     return y - v
 
-  def train(self,
-            training_data: np.array,
-            learning_rate: float = 0.01,
-            epochs: int = 100,
-            mini_batch_size: int = 4):
-    n = len(training_data)
-    m = max(epochs // 10, 1)
-
+  def train(self, training_data: np.array, test_data: np.array = None,
+            learning_rate: float = 0.01, epochs: int = 100,
+            mini_batch_size: int = 10):
+    """
+    Stochastic Gradient Descent (SGD) algorithm.
+    """
     for epoch in range(1, epochs + 1):
       random.shuffle(training_data)
 
       mini_batches = [
         training_data[i: i + mini_batch_size]
-        for i in range(0, n, mini_batch_size)
+        for i in range(0, len(training_data), mini_batch_size)
       ]
 
       for mini_batch in mini_batches:
         self._train_mini_batch(mini_batch, learning_rate)
 
-      if epoch % m == 0:
+      if epoch % max(epochs // 10, 1) == 0:
         loss = self.total_cost(training_data)
-        print(f"[{epoch} / {epochs} epoch] loss = {loss:.5}")
+        acc = self.evaluate(test_data) / len(test_data)
+        print(f"[{epoch} / {epochs} epoch] loss = {loss:.5} acc = {acc:.3}")
 
-  def _train_mini_batch(self,
-                        mini_batch: list,
-                        learning_rate: float) -> None:
-    m = len(mini_batch)
+  def _train_mini_batch(self, mini_batch: list, learning_rate: float) -> None:
+    n = len(mini_batch)
     nabla_w = [np.zeros(w.shape) for w in self.weights]
     nabla_b = [np.zeros(b.shape) for b in self.biases]
 
@@ -72,9 +66,9 @@ class NN:
       nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
       nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
 
-    self.weights = [w - learning_rate * (nw / m)
+    self.weights = [w - learning_rate * (nw / n)
                     for w, nw in zip(self.weights, nabla_w)]
-    self.biases = [b - learning_rate * (nb / m)
+    self.biases = [b - learning_rate * (nb / n)
                    for b, nb in zip(self.biases, nabla_b)]
 
   def backprop(self, x: np.array, v: np.array):
@@ -92,7 +86,7 @@ class NN:
     nabla_w = [np.zeros(w.shape) for w in self.weights]
     nabla_b = [np.zeros(b.shape) for b in self.biases]
 
-    nabla_a[-1] = NN.cost_derivative(activations[-1], v)
+    nabla_a[-1] = Network.cost_derivative(activations[-1], v)
 
     for l in range(self.num_layers - 2, -1, -1):
       t = nabla_a[l + 1] * dsigmoid(preactivations[l + 1])
@@ -101,7 +95,12 @@ class NN:
       nabla_b[l] = t
       nabla_a[l] = np.dot(self.weights[l].T, t)
 
-    return (nabla_w, nabla_b)
+    return nabla_w, nabla_b
+
+  def evaluate(self, data):
+    results = [(np.argmax(self.feedforward(x)), np.argmax(v))
+                for (x, v) in data]
+    return sum(int(x == y) for (x, y) in results)
 
   def total_cost(self, data: np.array) -> float:
     n = len(data)
@@ -112,21 +111,14 @@ class NN:
     return cost / n
 
 def main():
-  np.random.seed(0)
-  nn = NN((3, 2, 1))
+  #np.random.seed(0)
+  net = Network([784, 30, 10])
 
-  x = np.array([0., 0.2, 0.4]).reshape(-1, 1)
-  v = np.array([0.3]).reshape(-1, 1)
+  training_data, test_data = mnist_load()
+  training_data = training_data[:2000]
 
-  training_data = [(x, v)]
-
-  nn.train(
-    training_data,
-    learning_rate=0.1,
-    epochs=1000
-  )
-
-  print(nn.feedforward(x))
+  net.train(training_data, test_data, learning_rate=2, mini_batch_size=20,
+            epochs=30)
 
 if __name__ == "__main__":
   main()
